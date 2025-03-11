5b6e760bf373c9282eb40cf1511424186f5040f3
DmytroLysachenko <dlysachenko98@gmail.com>
2025-03-11 13:51:58 +0100

    Addeded sorting logic on ingredient page

---
+++ src/components/react/CustomSelector.tsx
@@ -0,0 +1,51 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+// Mark as a Client Component
+
+interface SelectorProps {
+  id: string;
+  options: { value: string; label: string }[];
+  defaultValue?: string;
+  onValueChange?: (value: string) => void;
+  className?: string;
+}
+
+export function Selector({
+  id,
+  options,
+  defaultValue,
+  onValueChange,
+  className,
+}: SelectorProps) {
+  const handleValueChange = (value: string) => {
+    if (onValueChange) {
+      onValueChange(value); // Call the custom handler if provided
+    } else {
+      // Default behavior: Update the URL
+      const url = new URL(window.location.href);
+      url.searchParams.set(id, value);
+      window.location.replace(url.href);
+    }
+  };
+
+  return (
+    <Select defaultValue={defaultValue} onValueChange={handleValueChange}>
+      <SelectTrigger className={className}>
+        <SelectValue placeholder="Select an option" />
+      </SelectTrigger>
+      <SelectContent>
+        {options.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
